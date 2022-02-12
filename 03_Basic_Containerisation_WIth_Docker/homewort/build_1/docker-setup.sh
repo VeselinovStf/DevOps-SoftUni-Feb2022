b1f6880fdd3258654c5ed53052c422a5589f4b28
LP <veselinovStf@gmail.com>
2022-02-12 15:03:35 +0200

    Fixing bad command

--- 03_Basic_Containerisation_WIth_Docker/homewort/build_1/docker-setup.sh
+++ 03_Basic_Containerisation_WIth_Docker/homewort/build_1/docker-setup.sh
@@ -4,18 +4,18 @@ echo "* Add hosts ..."
 echo "192.168.99.100 docker.do1.lab docker" >> /etc/hosts
 
 echo "* Add Docker repository ..."
-dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 echo "* Install Docker ..."
-dnf install -y docker-ce docker-ce-cli containerd.io
+sudo dnf install -y docker-ce docker-ce-cli containerd.io
 
 echo "* Enable and start Docker ..."
-systemctl enable docker
-systemctl start docker
+sudo systemctl enable docker
+sudo systemctl start docker
 
 echo "* Firewall - open port 8080 ..."
-firewall-cmd --add-port=8080/tcp –permanent
-firewall-cmd --reload
+sudo sudo firewall-cmd --permanent --add-port=8080/tcp
+sudo firewall-cmd --reload
 
 echo "* Add vagrant user to docker group ..."
-usermod -aG docker vagrant
+sudo usermod -aG docker vagrant
